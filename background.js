a4d539d6d12d92c10a6ee6064c2b9e23d68c3a2b
jazzyjackson <colten.jackson@lookalive.software>
2021-03-30 01:06:33 -0400

    initial beginners tutorial

---
+++ background.js
@@ -0,0 +1,16 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+// activate on /u/ but not when url.parts.includes('buy')
+// 
+
+/*
+urlmatches bitclout.com?
+chrome.webNavigation.onCompleted.addListener(function() {
+    alert("This is my favorite website!");
+}, {url: [{urlMatches : 'https://www.google.com/'}]});
+*/
